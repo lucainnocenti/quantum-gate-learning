094089d37ab94299052ae85df0136903a0f9af64
Luca Innocenti <lukeinnocenti@gmail.com>
2017-01-02 20:40:08 +0000

    stuff

--- QubitNetwork.py
+++ QubitNetwork.py
@@ -6,84 +6,7 @@ import theano
 import theano.tensor as T
 import theano.tensor.slinalg
 import theano.tensor.nlinalg
-
-
-# theano.config.optimizer = 'None'
-def complexrandn(dim1, dim2):
-    """Generates an array of pseudorandom, normally chosen, complex numbers."""
-    big_matrix = np.random.randn(dim1, dim2, 2)
-    return big_matrix[:, :, 0] + 1.j * big_matrix[:, :, 1]
-
-
-def complex2bigreal(matrix):
-    """Takes an nxn complex matrix and returns a 2nx2n real matrix.
-
-    To avoid the problem of theano and similar libraries not properly
-    supporting the gradient of complex objects, we map every complex
-    nxn matrix U to a bigger 2nx2n real matrix defined as
-    [[Ur, -Ui], [Ui, Ur]], where Ur and Ui are the real and imaginary
-    parts of U.
-    """
-    row1 = np.concatenate((np.real(matrix), -np.imag(matrix)), axis=1)
-    row2 = np.concatenate((np.imag(matrix), np.real(matrix)), axis=1)
-    return np.concatenate((row1, row2), axis=0)
-
-
-def get_sigmas_index(indices):
-    """Takes a tuple and gives back a length-16 array with a single 1.
-
-    Parameters
-    ----------
-    indices: a tuple of two integers, each one between 0 and 3.
-
-    Examples
-    --------
-    >>> get_sigmas_index((1, 0))
-    array([ 0.,  0.,  0.,  0.,  1.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,
-            0.,  0.,  0.])
-    >>> get_sigmas_index((0, 3))
-    array([ 0.,  0.,  0.,  1.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,
-            0.,  0.,  0.])
-
-    """
-    all_zeros = np.zeros(4 * 4)
-    all_zeros[indices[0] * 4 + indices[1]] = 1.
-    return all_zeros
-
-
-def generate_ss_terms():
-    """Returns the tensor products of every combination of two sigmas.
-
-    Generates a list in which each element is the tensor product of two
-    Pauli matrices, multiplied by the imaginary unit 1j and converted
-    into big real form using complex2bigreal.
-    The matrices are sorted in natural order, so that for example the
-    3th element is the tensor product of sigma_0 and sigma_3 and the
-    4th element is the tensor product of sigma_1 and sigma_0.
-    """
-    sigmas = [qutip.qeye(2), qutip.sigmax(), qutip.sigmay(), qutip.sigmaz()]
-    sigma_pairs = []
-    for idx1 in range(4):
-        for idx2 in range(4):
-            term = qutip.tensor(sigmas[idx1], sigmas[idx2])
-            term = 1j * term.data.toarray()
-            sigma_pairs.append(complex2bigreal(term))
-    return np.asarray(sigma_pairs)
-
-
-def chars2pair(chars):
-    out_pair = []
-    for idx in range(len(chars)):
-        if chars[idx] == 'x':
-            out_pair.append(1)
-        elif chars[idx] == 'y':
-            out_pair.append(2)
-        elif chars[idx] == 'z':
-            out_pair.append(3)
-        else:
-            raise ValueError('chars must contain 2 characters, each of'
-                             'which equal to either x, y, or z')
-    return tuple(out_pair)
+from utils import chars2pair, complex2bigreal
 
 
 class QubitNetwork:
@@ -432,8 +355,8 @@ def sgd_optimization(learning_rate=0.13, n_epochs=100,
     print('Building the model...')
 
     net = QubitNetwork(num_qubits=4,
-                       interactions=('all', ['xx', 'zz']),
-                       self_interactions=('all', ['z']),
+                       interactions=('all', ['zz']),
+                       self_interactions=('all', ['x', 'y', 'z']),
                        system_qubits=[0, 1, 2])
 
     # Generate training dataset. In this case the target unitary is
@@ -496,19 +419,22 @@ def sgd_optimization(learning_rate=0.13, n_epochs=100,
             y: test_target_states
         }
     )
-    # grad = theano.function(
-    #     inputs=[index],
-    #     outputs=g_J,
-    #     givens={
-    #         x: states[index * batch_size: (index + 1) * batch_size],
-    #         y: target_states[index * batch_size: (index + 1) * batch_size]
-    #     }
-    # )
+    grad = theano.function(
+        inputs=[index],
+        outputs=g_J,
+        givens={
+            x: states[index * batch_size: (index + 1) * batch_size],
+            y: target_states[index * batch_size: (index + 1) * batch_size]
+        }
+    )
+    theano.printing.pydotprint(train_model, outfile='train_model.png',
+                               var_with_name_simple=True)
     print('Let\'s roll!')
     n_train_batches = states.get_value().shape[0] // batch_size
     for idx in range(100):
         print('Epoch {}, '.format(idx), end='')
         for minibatch_index in range(n_train_batches):
             minibatch_avg_cost = train_model(minibatch_index)
+            print('gradient: {}'.format(grad(minibatch_index)))
             # print('minibatch avg cost: {}'.format(minibatch_avg_cost))
         print(test_model())
