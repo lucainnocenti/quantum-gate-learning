317f0b0b31f01d535f841009fe9c86a3aabb88ce
Luca Innocenti <lukeinnocenti@gmail.com>
2017-08-30 13:07:18 +0100

    starting to build QubitNetworkHamiltonian object

---
+++ src/qubit_network/hamiltonian.py
@@ -0,0 +1,84 @@
+"""
+Compute the Hamiltonian of the system.
+"""
+import numpy as np
+import sympy
+
+import qutip
+
+
+def pauli_product(*args):
+    """
+    Return sympy.Matrix object represing product of Pauli matrices.
+
+    Examples
+    --------
+    >>> pauli_product(1, 1)
+    Matrix([[0, 0, 0, 1],
+            [0, 0, 1, 0],
+            [0, 1, 0, 0],
+            [1, 0, 0, 0]])
+    """
+    for arg in args:
+        try:
+            if not 0 <= arg <= 3:
+                raise ValueError('Each argument must be between 0 and 3.')
+        except TypeError:
+            raise ValueError('The inputs must be integers.')
+    n_qubits = len(args)
+    sigmas = [qutip.qeye(2), qutip.sigmax(), qutip.sigmay(), qutip.sigmaz()]
+    output_matrix = [None] * n_qubits
+    for idx, arg in enumerate(args):
+        output_matrix[idx] = sigmas[arg]
+    output_matrix = qutip.tensor(*output_matrix).data.toarray()
+    return sympy.Matrix(output_matrix)
+
+
+class QubitNetworkHamiltonian:
+    """Compute the Hamiltonian for the qubit network.
+
+    The Hamiltonian can be generated in several different ways, depending
+    on the arguments given.
+    Parameters
+    ----------
+    n_qubits : int,
+        Number of qubits in the network.
+    parameters : ???, optional
+        If given, it is used to use the parameters in some predefined
+        way. Possible values are:
+        - 'all': use all 1- and 2-qubit interactions, each one with a
+                 different parameter assigned.
+    """
+
+    def __init__(self, n_qubits=None, expr=None, parameters=None, topology=None):
+        self.matrices = None
+        self.free_parameters = None
+        # extract lists of parameters and matrices to which each is to
+        # be multiplied
+        if expr is not None:
+            self._parse_sympy_expr(expr)
+        else:
+            raise NotImplementedError('To be implemented')
+
+    def _parse_sympy_expr(self, expr):
+        """
+        Extract free parameters and matrix coefficients from sympy expr.
+        """
+        try:
+            self.free_parameters = expr.free_symbols
+            self.len = expr.shape[0]
+        except TypeError:
+            raise TypeError('`expr` must be a sympy MatrixSymbol object.')
+        # initialize the list of matrices to which each parameter is multiplied
+        self.matrices = []
+        # extract the matrix to which each element is multiplied
+        for parameter in self.free_parameters:
+            self.matrices.append(expr.diff(parameter))
+
+    def get_matrix(self):
+        """Return the Hamiltonian matrix as a sympy matrix object."""
+        # final_matrix = sympy.MatrixSymbol('H', *self.matrices[0].shape)
+        final_matrix = sympy.Matrix(np.zeros(self.matrices[0].shape))
+        for matrix, parameter in zip(self.matrices, self.free_parameters):
+            final_matrix += parameter * matrix
+        return final_matrix
